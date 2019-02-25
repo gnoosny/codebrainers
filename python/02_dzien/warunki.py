20db889fed697f64c883f6e533305e71e4a3f27c
gnoosny <gnoosny@gmail.com>
2019-02-25 20:29:53 +0100

    third commit
    first python file

---
+++ python/02_dzien/warunki.py
@@ -0,0 +1,27 @@
+liczba1 = 4
+ulamek1 = 0.3
+lista1 = ['kot', 'pies', 'zyrafa', 'slon', 'lew']
+tekst1 = "To jest przykladowy tekst"
+logika1 = True
+none1 = None 
+
+liczba2 = 48
+ulamek2 = 0.436457
+lista2 = ['ford', 'kia', 'porsche', 'ferrari', 'polonez']
+tekst2 = "To jest drugi przykladowy tekst"
+logika2 = False
+none2 = None
+
+print(liczba1)
+print(ulamek1)
+print(lista1)
+print(tekst1)
+print(logika1)
+print(none1)
+
+print(liczba2)
+print(ulamek2)
+print(lista2)
+print(tekst2)
+print(logika2)
+print(none2)
