d1562c5e7cee746d7f28c7d78f5627cf15a53735
gnoosny <gnoosny@gmail.com>
2019-02-27 20:27:05 +0100

    6th commit loop

---
+++ python/03_dzien/petla.py
@@ -0,0 +1,39 @@
+#for x in range(5):
+#	print(x)
+
+#for x in range(5):
+#	if x<5: 
+#		print(x)
+#	elif x == 5: 
+#		print(x + "\n")
+
+#lista = ['P', 'r', 'z', 'e', 'm', 'e', 'k']
+#for znak in lista:
+#	print(znak , "\n")
+	
+#imie = "Przemek"
+#for znak in imie:
+#	print(znak)
+#print(imie[2])
+	
+#for x in xrange(1,5):
+#	print(x)
+
+#lista = ['String', None, 1, 0.14234, 'True', [1, 2, 3] ]
+
+#for i in lista:
+#	print(i)
+
+for i in range(21):
+	print("liczba:", i, "\tkwadrat ", i**2, "\tpierwiastek ", i**(1/2))
+	
+for i in range(21):
+	print("liczba:" + str(i) + "\tkwadrat " + str(i**2) + "\tpierwiastek " + str(i**(1/2)))
+	
+	
+	
+	
+	
+	
+	
+	
