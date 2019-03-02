42ebc46c51d152a577c53767c27e195ea2c5c09b
gnoosny <gnoosny@gmail.com>
2019-03-02 16:51:56 +0100

    10th commit
    funkcja fibonacci

--- python/04_dzien/function_fibonacci.py
+++ python/04_dzien/function_fibonacci.py
@@ -1,7 +1,12 @@
-def fibonacci(m):
-	print(m+1)
-	return m+2
+m = input("wypisz m: ")
+m = int(m)
 
-wartosc = fibonacci(15)
+def fibonacci(m):
+	list = [0, 1]
+	for i in range(2, m+1):
+		list.append(list[i-1] + list[i-2])
+	return list[m], list
+
+wartosc = fibonacci(m)
 
 print(wartosc)
---
+++ python/04_dzien/my_functions.py
@@ -0,0 +1,6 @@
+def iloczyn(a, b, c, d):
+	wynik = a * b * c * d
+	return wynik
+	
+sum = iloczyn(3, 8, 3, 1)
+print(sum)
