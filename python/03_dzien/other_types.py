0f0dc303e4ddfbcca4f7973797906cacf467ee23
gnoosny <gnoosny@gmail.com>
2019-03-02 10:01:57 +0100

    6th commit
    04 dzien

---
+++ python/03_dzien/other_types.py
@@ -0,0 +1,8 @@
+krotka = (1, 2, 3, 4, 5)
+
+slownik = {
+	"haslo": "definicja",
+	"amendment": "poprawka"
+}
+
+slownik["haslo"]
---
+++ python/04_dzien/findinlist.py
@@ -0,0 +1,5 @@
+lista = ['kot', 'pies', 3, 5.4535, None, True, False, 'zyrafa']
+
+for i in lista:
+	if i == 'pies':
+		print('znalazlem psa', i)
