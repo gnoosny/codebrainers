f7e713aa47ae94e3351e43518c7695c75b2aa71c
gnoosny <gnoosny@gmail.com>
2019-03-06 20:53:06 +0100

    16th commit
    quesing game

--- python/05_dzien/guesing_number.py
+++ python/05_dzien/guesing_number.py
@@ -1,25 +1,33 @@
 import random
 number = random.randrange(0, 101, 1)
 
-print(number)
-
+# print(number)
 
 
 guesed = False
 tries = 0
 
 while (not guesed) and (tries < 7):
-	g = input("podaj jakas liczbe pomiedzy 0 a 100: ")
-	g = int(g)
-	if g > number:
+	try:
+		g = input("podaj jakas liczbe pomiedzy 0 a 100: ")
+		g = int(g)
+	except KeyboardInterrupt:
+		print("\n\nBaby come back! <3")
+		break
+
+	if tries == 6:
+ 		print("You didn't guesed it")
+ 		break
+	elif g > number:
  		print("the number that I choosed is smaller")
  		tries += 1
 	elif g < number:
  		print("the number that I choosed is bigger")
  		tries += 1
 	elif g == number:
- 		print("You've guesed it, the number I choosed is " + str(number))
- 		guesed = True
+		print("You've guesed it, the number I choosed is " + str(number))
+		guesed = True
+	
 
  		
  		## zgadles w x probie
