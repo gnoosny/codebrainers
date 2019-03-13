ce033aca55dd25213fd441b7d152416d3288b012
Przemyslaw Leksa <38156110+gnoosny@users.noreply.github.com>
2019-03-13 16:49:32 +0100

    Update hangman3.py

--- python/08_dzien/hangman3.py
+++ python/08_dzien/hangman3.py
@@ -12,6 +12,7 @@ print(title)
 print("Space at: ", space)
 
 password = title.lower()
+password_change = password
 
 print("psw: " + password)
 
@@ -20,10 +21,10 @@ print(password_len)
 hash = ""
 
 if space < 0:
-  hash = "X" * password_len
+  hash = "#" * password_len
   print(hash)
 elif space > 0:
-  hash = "X" * space + " " + "X" * (password_len - space - 1)
+  hash = "#" * space + " " + "#" * (password_len - space - 1)
   print(hash)
 
 letter = input("enter a letter: ")
@@ -31,13 +32,21 @@ letter = input("enter a letter: ")
 def search(letter, hash):
   #for number, letter in enumerate(password):
     hash_new = hash
-    if letter in password:
-      print("yes")
-      print(password.index(letter))
-      print(hash_new[0:password.index(letter)] + letter + hash_new[password.index(letter):password_len])
-      hash_new = hash_new[0:password.index(letter)] + letter + hash_new[password.index(letter):password_len]
+    if letter in password_change:
+      print("\nI've guesed the letter!")
+      print(password_change.index(letter))
+      
+      hash_new = list(hash_new)
+      hash_new[password_change.index(letter)] = letter
+      hash_new = "".join(hash_new)
+
+      print(hash_new)
+      if hash_new == password:
+        print("Congratulations you've won!")
       letter = input("enter a letter: ")
       search(letter, hash_new)
     else:
-      print("no")
+      print("\nThere is no " + letter + " in the password")
+      letter = input("enter a letter: ")
       search(letter, hash_new)
+print(search(letter, hash))
